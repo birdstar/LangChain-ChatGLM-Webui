887fd51affa0564991f6d9aef95f62f25bff87b5
birdstar <birdstar@gmail.com>
2023-05-23 14:59:25 +0800

    add

--- jina_serving.py
+++ jina_serving.py
@@ -241,13 +241,14 @@ def reinit_model(large_language_model: str, embedding_model: str):
 
 
 @serving
-# def vector_store(file_path: str or List[str]):
-def vector_store(file_path: str):
+def vector_store(file_path: str or List[str]):
+# def vector_store(file_path: str):
     print(0)
-    # vector_store_state, loaded_files = knowladge_based_chat_llm.init_knowledge_vector_store(
-    #     file_path)
+    vector_store_state, loaded_files = knowladge_based_chat_llm.init_knowledge_vector_store(
+        file_path)
     # return vector_store_state
     return 1
+
 
 
 @serving
